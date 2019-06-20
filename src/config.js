3ad76c776e69a2b847d42157f6ff6841cf59fd65
Adebayo Adeniyan <adebeslick@gmail.com>
2019-06-20 17:58:10 +0100

    config missing bug fix

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const darkSkyToken = '73c631ff9457514f51f504eaeb5f385a';
+const mapBoxToken = 'pk.eyJ1IjoiYWQzYmF5MCIsImEiOiJjangyZThmczQwMGxkNGJxam1kanRycDVrIn0.gvVR5ayAcyKRNcZsAEfuZA'
+
+   module.exports = {
+       darkSkyToken,mapBoxToken
+   }
+  
